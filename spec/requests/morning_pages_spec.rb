fd1a0fe1257b15395a6fdb9435d4ff437c4295dd
Nikki Thean <nikki.thean@gmail.com>
2013-11-01 15:50:47 -0400

    Add a full_title helper

--- spec/requests/morning_pages_spec.rb
+++ spec/requests/morning_pages_spec.rb
@@ -15,8 +15,12 @@ describe "Morning Pages" do
   	end
 
   	it "should have the right title" do
-  		expect(page).to have_title("#{base_title} | Home")
+  		expect(page).to have_title("Morning Pages")
   	end
+
+    it "should not have a custom page title" do
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Help page" do
