32a808d4f1e08a9ac6df09cb21ee73cc076ddec3
Nikki Thean <nikki.thean@gmail.com>
2013-11-06 12:39:22 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 MorningPages::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'morning_pages#home'
 
   match '/about', to: 'morning_pages#about', via: 'get'
   match '/contact', to: 'morning_pages#contact', via: 'get'
   match '/help', to: 'morning_pages#help', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
